397cecfd1d90cd46fdc012756596e8538edd8f55
cai.zhiwei <cai.zhiwei@gmail.com>
2014-12-01 21:54:14 -0500

    initial checkin

---
+++ myproject/erfpacket/erfpacket/erffilesniff.cpp
@@ -0,0 +1,44 @@
+#include "erffilesniff.h"
+#include "tins\ethernetII.h"
+
+using namespace Tins;
+
+
+ErfFileSniff::ErfFileSniff(const std::string& file_name)
+{
+	infile.open(file_name, ios::binary | ios::in);
+}
+
+
+ErfFileSniff::~ErfFileSniff()
+{
+	infile.close();
+}
+
+
+PtrPacket ErfFileSniff::next_packet()
+{
+	struct timeval tv;
+	PDU *pdu;	
+	ERF_HEAD hd;
+	
+	infile.read(reinterpret_cast <char*>(&hd), sizeof(hd));
+
+	if (infile)
+	{
+		hd.ntoh();
+		if (!hd.IsValid()) throw "Invalid ERF header";
+		if (hd.GetType() != TYPE_ETH) throw unknown_link_type();
+
+		tv.tv_sec = hd.seconds;
+		tv.tv_usec = hd.nanoseconds;
+
+		char buffer[19200];
+
+		infile.read(buffer, (hd.rlen - sizeof(hd)));
+		pdu = new EthernetII(reinterpret_cast <const uint8_t*>(buffer + sizeof(ETH_HEAD)), hd.wlen);
+		return PtrPacket(pdu, tv);			
+	}
+	else
+		return PtrPacket(nullptr, tv);
+}
---
+++ myproject/erfpacket/erfpacket/erffilesniff.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <string>
+#include <tins/packet.h>
+#include <fstream>
+
+using namespace Tins;
+using namespace std;
+
+
+#define TYPE_ETH 2
+
+#pragma pack(push,1)
+struct ERF_HEAD
+{
+	unsigned long nanoseconds;
+	unsigned long seconds;
+	char type;
+	char flag;
+	short rlen;
+	short lctr;
+	short wlen;
+
+	void ntoh()
+	{
+		wlen = ntohs(wlen);
+		rlen = ntohs(rlen);	
+
+		double tmp1 = nanoseconds;
+		double tmp2 = 1000000000.0 / (65536.0 * 65536.0);
+		nanoseconds = (unsigned long)(tmp1 * tmp2 + 0.5);
+	}
+
+	bool IsValid()
+	{	
+		if (wlen >= rlen) return false;
+		return true;
+	}
+
+	int GetType()
+	{
+		return type & 0x7f;
+	}
+};
+
+struct ETH_HEAD
+{
+	char offset;
+	char reserved;
+};
+
+#pragma pack(pop)
+
+class ErfFileSniff
+{
+public:
+	ErfFileSniff(const std::string &file_name);
+	~ErfFileSniff();
+
+	PtrPacket next_packet();
+
+
+
+private:
+	ifstream infile;
+};
+
